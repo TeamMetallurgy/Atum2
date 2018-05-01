1088696080243cc946ac7f2bcb1bc8650fc2f8a8
GirafiStudios <girafi0@gmail.com>
2018-05-01 19:49:28 +0200

    Change some constants to Atum 2

--- src/main/java/com/teammetallurgy/atum/utils/Constants.java
+++ src/main/java/com/teammetallurgy/atum/utils/Constants.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 public class Constants {
 
     public static final String MOD_ID = "atum";
-    public static final String MOD_NAME = "Atum";
+    public static final String MOD_NAME = "Atum2";
     public static final String VERSION = "@VERSION@";
     public static final String CLIENT = "com.teammetallurgy.atum.proxy.ClientProxy";
     public static final String SERVER = "com.teammetallurgy.atum.proxy.CommonProxy";
