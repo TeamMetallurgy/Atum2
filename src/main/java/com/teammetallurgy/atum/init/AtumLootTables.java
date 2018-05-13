c8df64a71a279c114b24c563f059c152d649336c
GirafiStudios <girafi0@gmail.com>
2018-05-13 22:06:24 +0200

    Added pharaoh loot table

--- src/main/java/com/teammetallurgy/atum/init/AtumLootTables.java
+++ src/main/java/com/teammetallurgy/atum/init/AtumLootTables.java
@@ -7,6 +7,7 @@ import net.minecraft.world.storage.loot.LootTableList;
 public class AtumLootTables {
     public static final ResourceLocation FISH = register("gameplay/fishing/fish");
     public static final ResourceLocation RUINS = register("chests/ruins");
+    public static final ResourceLocation PHARAOH = register("chests/pharaoh");
 
     private static ResourceLocation register(String path) {
         return LootTableList.register(new ResourceLocation(Constants.MOD_ID, path));
