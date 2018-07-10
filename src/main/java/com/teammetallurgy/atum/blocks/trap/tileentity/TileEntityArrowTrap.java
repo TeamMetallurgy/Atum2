16e57f2454ecfd584870f189c197ab2bad57ba6b
GirafiStudios <faj10@msn.com>
2018-07-11 00:17:06 +0200

    Somewhat shitty implementation of Arrow Trap, Closes #53

--- src/main/java/com/teammetallurgy/atum/blocks/trap/tileentity/TileEntityArrowTrap.java
+++ src/main/java/com/teammetallurgy/atum/blocks/trap/tileentity/TileEntityArrowTrap.java
@@ -1,11 +1,7 @@
 package com.teammetallurgy.atum.blocks.trap.tileentity;
 
 import com.teammetallurgy.atum.blocks.trap.BlockTrap;
-import net.minecraft.dispenser.IBlockSource;
-import net.minecraft.dispenser.IPosition;
-import net.minecraft.dispenser.PositionImpl;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.IProjectile;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityTippedArrow;
@@ -15,7 +11,6 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
@@ -26,40 +21,24 @@ public class TileEntityArrowTrap extends TileEntityTrap implements ITickable {
 
     @Override
     public void update() {
-        if (timer > 0) timer --;
         int range = 6;
         EntityPlayer player = world.getClosestPlayer((double) getPos().getX(), (double) getPos().getY(), (double) getPos().getZ(), range, false);
-        if (!this.isDisabled && player != null && timer == 0 /*&& !player.capabilities.isCreativeMode*/) {
-            EnumFacing facing = world.getBlockState(pos).getValue(BlockTrap.FACING);
-            List<EntityPlayer> players = world.getEntitiesWithinAABB(EntityPlayer.class, getFacingBoxWithRange(facing, range));
-            for (EntityPlayer p : players) {
-                if (p != null) {
-                    timer = 80;
-                    this.fireArrow(facing); //TODO add 4 second timer
+        if (!this.isDisabled && player != null && !player.capabilities.isCreativeMode) {
+            if (timer > 0) timer --;
+            if (timer == 0) {
+                EnumFacing facing = world.getBlockState(pos).getValue(BlockTrap.FACING);
+                List<EntityLivingBase> players = world.getEntitiesWithinAABB(EntityLivingBase.class, getFacingBoxWithRange(facing, range));
+                for (EntityLivingBase p : players) {
+                    if (p != null) {
+                        timer = 80;
+                        this.fire(facing);
+                    }
                 }
             }
         }
     }
 
-    private EntityArrow getArrow(World world, IPosition pos) {
-        EntityTippedArrow arrow = new EntityTippedArrow(world, pos.getX(), pos.getY(), pos.getZ());
-        arrow.pickupStatus = EntityArrow.PickupStatus.DISALLOWED;
-        return arrow;
-    }
-
-    private IPosition getFirePosition(EnumFacing facing, BlockPos pos) {
-        double x = pos.getX() + 0.7D * (double) facing.getFrontOffsetX();
-        double y = pos.getY() + 0.7D * (double) facing.getFrontOffsetY();
-        double z = pos.getZ() + 0.7D * (double) facing.getFrontOffsetZ();
-        return new PositionImpl(x, y, z);
-    }
-
-    private void fireArrow(EnumFacing facing) {
-        IPosition iPos = getFirePosition(facing, getPos());
-        IProjectile arrow = getArrow(world, iPos);
-        arrow.shoot((double) facing.getFrontOffsetX(), (double) ((float) facing.getFrontOffsetY() + 0.1F), (double) facing.getFrontOffsetZ(), 1.1F, 6.0F);
-        world.spawnEntity((Entity) arrow);
-
+    private void fire(EnumFacing facing) {
         double x = (double) pos.getX() + 0.5D;
         double y = (double) pos.getY() + world.rand.nextDouble() * 12.0D / 16.0D;
         double z = (double) pos.getZ() + 0.5D;
@@ -69,23 +48,37 @@ public class TileEntityArrowTrap extends TileEntityTrap implements ITickable {
 
         switch (facing) {
             case DOWN:
+                fireArrow(world, facing, x - randomPos, y - 0.5D, z);
                 world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x - randomPos, y - 0.2D, z, 0.0D, 0.0D, 0.0D);
                 break;
             case UP:
+                fireArrow(world, facing, x - randomPos, y + 1.0D, z);
                 world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x - randomPos, y + 1.0D, z, 0.0D, 0.0D, 0.0D);
                 break;
             case WEST:
+                fireArrow(world, facing, x - 0.52D, y, z + randomPos);
                 world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x - 0.52D, y, z + randomPos, 0.0D, 0.0D, 0.0D);
                 break;
             case EAST:
+                fireArrow(world, facing, x + 0.52D, y, z + randomPos);
                 world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x + 0.52D, y, z + randomPos, 0.0D, 0.0D, 0.0D);
                 break;
             case NORTH:
+                fireArrow(world, facing, x + randomPos, y, z - 0.52D);
                 world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x + randomPos, y, z - 0.52D, 0.0D, 0.0D, 0.0D);
                 break;
             case SOUTH:
+                fireArrow(world, facing, x + randomPos, y, z + 0.52D);
                 world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x + randomPos, y, z + 0.52D, 0.0D, 0.0D, 0.0D);
                 break;
+        }
+    }
+
+    private void fireArrow(World world, EnumFacing facing, double x, double y, double z) {
+        if (!world.isRemote) {
+            EntityArrow arrow = new EntityTippedArrow(world, x, y, z);
+            arrow.shoot((double) facing.getFrontOffsetX(), (double) ((float) facing.getFrontOffsetY() + 0.1F), (double) facing.getFrontOffsetZ(), 1.1F, 6.0F);
+            world.spawnEntity(arrow);
         }
     }
 
