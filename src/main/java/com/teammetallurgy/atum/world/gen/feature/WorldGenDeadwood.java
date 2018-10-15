fd239c645ac7e476f83f8be2fc470536de73bfa0
GirafiStudios <girafi0@gmail.com>
2018-10-16 01:31:21 +0200

    Added proper checks for deadwood tree generation

--- src/main/java/com/teammetallurgy/atum/world/gen/feature/WorldGenDeadwood.java
+++ src/main/java/com/teammetallurgy/atum/world/gen/feature/WorldGenDeadwood.java
@@ -1,5 +1,6 @@
 package com.teammetallurgy.atum.world.gen.feature;
 
+import com.teammetallurgy.atum.blocks.BlockStrangeSand;
 import com.teammetallurgy.atum.init.AtumBlocks;
 import com.teammetallurgy.atum.utils.Constants;
 import net.minecraft.server.MinecraftServer;
@@ -9,7 +10,7 @@ import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraft.world.gen.feature.WorldGenAbstractTree;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
@@ -18,8 +19,12 @@ import net.minecraft.world.gen.structure.template.TemplateManager;
 import javax.annotation.Nonnull;
 import java.util.Random;
 
-public class WorldGenDeadwood extends WorldGenerator {
+public class WorldGenDeadwood extends WorldGenAbstractTree {
     private static final ResourceLocation TREES = new ResourceLocation(Constants.MOD_ID, "deadwoodtrees/deadwoodtree_");
+
+    public WorldGenDeadwood(boolean notify) {
+        super(notify);
+    }
 
     @Override
     public boolean generate(@Nonnull World world, @Nonnull Random rand, @Nonnull BlockPos pos) {
@@ -48,8 +53,13 @@ public class WorldGenDeadwood extends WorldGenerator {
             placeCheck = placeCheck.up();
         }
 
-        BlockPos zero = template.getZeroPositionWithTransform(placeCheck, Mirror.NONE, rotation);
-        template.addBlocksToWorld(world, zero, settings, 20);
-        return true;
+
+        if (world.canSeeSky(placeCheck) && (this.isReplaceable(world, placeCheck) || world.getBlockState(placeCheck.down()).getBlock() instanceof BlockStrangeSand)) {
+            System.out.println("CanPlace");
+            BlockPos zero = template.getZeroPositionWithTransform(placeCheck, Mirror.NONE, rotation);
+            template.addBlocksToWorld(world, zero, settings, 20);
+            return true;
+        }
+        return false;
     }
 }
