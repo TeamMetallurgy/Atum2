0539c16eaf7e882a067fc14d35dea3bd08af0bb9
Austin Smith <Shadowclaimer2@gmail.com>
2018-10-27 23:34:35 -0400

    Nerf to Nomads

--- src/main/java/com/teammetallurgy/atum/entity/bandit/EntityNomad.java
+++ src/main/java/com/teammetallurgy/atum/entity/bandit/EntityNomad.java
@@ -54,10 +54,10 @@ public class EntityNomad extends EntityBanditBase implements IRangedAttackMob {
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(15.0D);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
-        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(36.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(2.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(24.0D);
     }
 
     @Override
