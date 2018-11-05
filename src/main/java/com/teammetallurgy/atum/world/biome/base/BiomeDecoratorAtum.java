d4e4fc9d847cbc44c916612f73ff42ea7834a32a
GirafiStudios <girafi0@gmail.com>
2018-11-05 03:39:23 +0100

    Simplified ore gen config. Still need support for min/max height & veins per chunk

--- src/main/java/com/teammetallurgy/atum/world/biome/base/BiomeDecoratorAtum.java
+++ src/main/java/com/teammetallurgy/atum/world/biome/base/BiomeDecoratorAtum.java
@@ -17,6 +17,7 @@ import net.minecraft.world.biome.BiomeDecorator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType;
 import net.minecraftforge.event.terraingen.OreGenEvent;
@@ -25,6 +26,7 @@ import net.minecraftforge.event.terraingen.OreGenEvent.Pre;
 import net.minecraftforge.event.terraingen.TerrainGen;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 import java.util.Random;
 
 public class BiomeDecoratorAtum extends BiomeDecorator {
@@ -38,43 +40,41 @@ public class BiomeDecoratorAtum extends BiomeDecorator {
 
     BiomeDecoratorAtum() {
         super();
-        this.dirtGen = generateMinable(AtumBlocks.SAND.getDefaultState(), 32);
-        this.gravelGen = generateMinable(AtumBlocks.LIMESTONE_GRAVEL.getDefaultState(), 32);
-        this.clayGen = generateMinable(AtumBlocks.SANDY_CLAY.getDefaultState(), 12);
-        this.alabasterGen = generateMinable(AtumBlocks.ALABASTER.getDefaultState(), 33);
-        this.porphyryGen = generateMinable(AtumBlocks.PORPHYRY.getDefaultState(), 33);
-        this.limestoneScarab = generateMinable(AtumBlocks.LIMESTONE.getDefaultState().withProperty(BlockLimestone.HAS_SCARAB, true), 10);
-        if (AtumConfig.COAL_ENABLED) {
-            this.coalGen = generateMinable(AtumBlocks.COAL_ORE.getDefaultState(), AtumConfig.COAL_VEIN);
-        }
-        if (AtumConfig.IRON_ENABLED) {
-            this.ironGen = generateMinable(AtumBlocks.IRON_ORE.getDefaultState(), AtumConfig.IRON_VEIN);
-        }
-        if (AtumConfig.GOLD_ENABLED) {
-            this.goldGen = generateMinable(AtumBlocks.GOLD_ORE.getDefaultState(), AtumConfig.GOLD_VEIN);
-        }
-        if (AtumConfig.REDSTONE_ENABLED) {
-            this.redstoneGen = generateMinable(AtumBlocks.REDSTONE_ORE.getDefaultState(), AtumConfig.REDSTONE_VEIN);
-        }
-        if (AtumConfig.DIAMOND_ENABLED) {
-            this.diamondGen = generateMinable(AtumBlocks.DIAMOND_ORE.getDefaultState(), AtumConfig.DIAMOND_VEIN);
-        }
-        if (AtumConfig.EMERALD_ENABLED) {
-            this.emeraldGen = generateMinable(AtumBlocks.EMERALD_ORE.getDefaultState(), AtumConfig.EMERALD_VEIN);
-        }
-        if (AtumConfig.LAPIS_ENABLED) {
-            this.lapisGen = generateMinable(AtumBlocks.LAPIS_ORE.getDefaultState(), AtumConfig.LAPIS_VEIN);
-        }
-        this.boneGen = generateMinable(AtumBlocks.BONE_ORE.getDefaultState(), 8);
-        this.relicGen = generateMinable(AtumBlocks.RELIC_ORE.getDefaultState(), 4);
+        //Atum ores
+        this.alabasterGen = generateMineable(AtumBlocks.ALABASTER.getDefaultState(), 33);
+        this.porphyryGen = generateMineable(AtumBlocks.PORPHYRY.getDefaultState(), 33);
+        this.limestoneScarab = generateMineable(AtumBlocks.LIMESTONE.getDefaultState().withProperty(BlockLimestone.HAS_SCARAB, true), 10);
+        this.boneGen = generateMineable(AtumBlocks.BONE_ORE.getDefaultState(), 8);
+        this.relicGen = generateMineable(AtumBlocks.RELIC_ORE.getDefaultState(), 4);
+
+        //Vanilla based ores
+        this.coalGen = generateMineable(AtumBlocks.COAL_ORE.getDefaultState(), 16);
+        this.ironGen = generateMineable(AtumBlocks.IRON_ORE.getDefaultState(), 8);
+        this.goldGen = generateMineable(AtumBlocks.GOLD_ORE.getDefaultState(), 8);
+        this.redstoneGen = generateMineable(AtumBlocks.REDSTONE_ORE.getDefaultState(), 7);
+        this.diamondGen = generateMineable(AtumBlocks.DIAMOND_ORE.getDefaultState(), 7);
+        this.emeraldGen = generateMineable(AtumBlocks.EMERALD_ORE.getDefaultState(), 5);
+        this.lapisGen = generateMineable(AtumBlocks.LAPIS_ORE.getDefaultState(), 6);
+
+        //Noise
+        this.dirtGen = generateMineable(AtumBlocks.SAND.getDefaultState(), 32);
+        this.gravelGen = generateMineable(AtumBlocks.LIMESTONE_GRAVEL.getDefaultState(), 32);
+        this.clayGen = generateMineable(AtumBlocks.SANDY_CLAY.getDefaultState(), 12);
 
         this.treesPerChunk = 0;
         this.shrubChance = 0.3F;
         this.generateFalls = false;
     }
 
-    private WorldGenMinable generateMinable(IBlockState state, int size) {
-        return new WorldGenMinable(state, size, BlockMatcher.forBlock(AtumBlocks.LIMESTONE));
+    private WorldGenMinable generateMineable(IBlockState state, int size) {
+        String category = AtumConfig.OREGEN + Configuration.CATEGORY_SPLITTER + Objects.requireNonNull(state.getBlock().getRegistryName()).getPath();
+        size = AtumConfig.config.get(category, "vein size", size).getInt();
+        AtumConfig.config.save();
+        if (size > 0) {
+            return new WorldGenMinable(state, size, BlockMatcher.forBlock(AtumBlocks.LIMESTONE));
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -86,6 +86,71 @@ public class BiomeDecoratorAtum extends BiomeDecorator {
             this.genDecorations(biomeGenBase, world, random);
             this.decorating = false;
         }
+    }
+
+    @Override
+    protected void generateOres(@Nonnull World world, @Nonnull Random random) {
+        MinecraftForge.ORE_GEN_BUS.post(new OreGenEvent.Pre(world, random, chunkPos));
+        if (TerrainGen.generateOre(world, random, this.coalGen, chunkPos, OreGenEvent.GenerateMinable.EventType.COAL)) {
+            this.genStandardOre1(world, random, 20, this.coalGen, 0, 128);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.ironGen, chunkPos, OreGenEvent.GenerateMinable.EventType.IRON)) {
+            this.genStandardOre1(world, random, 20, this.ironGen, 0, 62);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.goldGen, chunkPos, OreGenEvent.GenerateMinable.EventType.GOLD)) {
+            this.genStandardOre1(world, random, 2, this.goldGen, 0, 32);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.redstoneGen, chunkPos, OreGenEvent.GenerateMinable.EventType.REDSTONE)) {
+            this.genStandardOre1(world, random, 8, this.redstoneGen, 0, 16);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.emeraldGen, chunkPos, OreGenEvent.GenerateMinable.EventType.EMERALD)) {
+            this.genStandardOre1(world, random, 3, this.emeraldGen, 4, 32);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.diamondGen, chunkPos, OreGenEvent.GenerateMinable.EventType.DIAMOND)) {
+            this.genStandardOre1(world, random, 1, this.diamondGen, 0, 16);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.lapisGen, chunkPos, OreGenEvent.GenerateMinable.EventType.LAPIS)) {
+            this.genStandardOre2(world, random, 1, this.lapisGen, 16, 16);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.boneGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+            this.genStandardOre1(world, random, 6, this.boneGen, 0, 62);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.relicGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+            this.genStandardOre1(world, random, 3, this.relicGen, 0, 62);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.dirtGen, chunkPos, OreGenEvent.GenerateMinable.EventType.DIRT)) {
+            this.genStandardOre1(world, random, 32, this.dirtGen, 0, 256);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.gravelGen, chunkPos, OreGenEvent.GenerateMinable.EventType.GRAVEL)) {
+            this.genStandardOre1(world, random, 10, this.gravelGen, 0, 256);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.clayGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+            this.genStandardOre1(world, random, 8, this.clayGen, 0, 50);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.alabasterGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+            this.genStandardOre1(world, random, 10, this.alabasterGen, 0, 60);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.porphyryGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+            this.genStandardOre1(world, random, 10, this.porphyryGen, 0, 60);
+        }
+
+        if (TerrainGen.generateOre(world, random, this.limestoneScarab, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
+            this.genStandardOre1(world, random, 8, this.limestoneScarab, 11, 80);
+        }
+        MinecraftForge.ORE_GEN_BUS.post(new OreGenEvent.Post(world, random, chunkPos));
     }
 
     @Override
@@ -156,70 +221,5 @@ public class BiomeDecoratorAtum extends BiomeDecorator {
             }
         }
         MinecraftForge.EVENT_BUS.post(new Post(world, random, chunkPos));
-    }
-
-    @Override
-    protected void generateOres(@Nonnull World world, @Nonnull Random random) {
-        MinecraftForge.ORE_GEN_BUS.post(new OreGenEvent.Pre(world, random, chunkPos));
-        if (TerrainGen.generateOre(world, random, this.coalGen, chunkPos, OreGenEvent.GenerateMinable.EventType.COAL)) {
-            this.genStandardOre1(world, random, 20, this.coalGen, 0, 128);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.ironGen, chunkPos, OreGenEvent.GenerateMinable.EventType.IRON)) {
-            this.genStandardOre1(world, random, 20, this.ironGen, 0, 62);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.goldGen, chunkPos, OreGenEvent.GenerateMinable.EventType.GOLD)) {
-            this.genStandardOre1(world, random, 2, this.goldGen, 0, 32);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.redstoneGen, chunkPos, OreGenEvent.GenerateMinable.EventType.REDSTONE)) {
-            this.genStandardOre1(world, random, 8, this.redstoneGen, 0, 16);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.emeraldGen, chunkPos, OreGenEvent.GenerateMinable.EventType.EMERALD)) {
-            this.genStandardOre1(world, random, MathHelper.getInt(random, 3, 8), this.emeraldGen, 4, 32);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.diamondGen, chunkPos, OreGenEvent.GenerateMinable.EventType.DIAMOND)) {
-            this.genStandardOre1(world, random, 1, this.diamondGen, 0, 16);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.lapisGen, chunkPos, OreGenEvent.GenerateMinable.EventType.LAPIS)) {
-            this.genStandardOre2(world, random, 1, this.lapisGen, 16, 16);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.boneGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            this.genStandardOre1(world, random, 6, this.boneGen, 0, 62);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.relicGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            this.genStandardOre1(world, random, 3, this.relicGen, 0, 62);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.dirtGen, chunkPos, OreGenEvent.GenerateMinable.EventType.DIRT)) {
-            this.genStandardOre1(world, random, 32, this.dirtGen, 0, 256);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.gravelGen, chunkPos, OreGenEvent.GenerateMinable.EventType.GRAVEL)) {
-            this.genStandardOre1(world, random, 10, this.gravelGen, 0, 256);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.clayGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            this.genStandardOre1(world, random, 8, this.clayGen, 0, 50);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.alabasterGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            this.genStandardOre1(world, random, 10, this.alabasterGen, 0, 60);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.porphyryGen, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            this.genStandardOre1(world, random, 10, this.porphyryGen, 0, 60);
-        }
-
-        if (TerrainGen.generateOre(world, random, this.limestoneScarab, chunkPos, OreGenEvent.GenerateMinable.EventType.CUSTOM)) {
-            this.genStandardOre1(world, random, 8, this.limestoneScarab, 0, 60);
-        }
-        MinecraftForge.ORE_GEN_BUS.post(new OreGenEvent.Post(world, random, chunkPos));
     }
 }
