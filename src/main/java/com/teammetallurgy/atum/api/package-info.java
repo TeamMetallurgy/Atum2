5ee043c5573f2002c167ef5ace9e2594763320cf
GirafiStudios <girafi0@gmail.com>
2018-11-28 09:18:08 +0100

    Initial API stuff

---
+++ src/main/java/com/teammetallurgy/atum/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = Constants.MOD_ID, apiVersion = "@API_VERSION@", provides = "AtumAPI")
+package com.teammetallurgy.atum.api;
+
+import com.teammetallurgy.atum.utils.Constants;
+import net.minecraftforge.fml.common.API;
