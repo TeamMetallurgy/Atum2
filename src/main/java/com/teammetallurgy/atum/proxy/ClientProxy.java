92d27f4a5305244163dc5e1e7ae15887514abfb7
GirafiStudios <faj10@msn.com>
2018-12-29 05:03:40 +0100

    Fixed wolf gui when ridden being client-side only

--- src/main/java/com/teammetallurgy/atum/proxy/ClientProxy.java
+++ src/main/java/com/teammetallurgy/atum/proxy/ClientProxy.java
@@ -57,7 +57,6 @@ import net.minecraft.world.ColorizerFoliage;
 import net.minecraft.world.biome.BiomeColorHelper;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -102,7 +101,6 @@ public class ClientProxy extends CommonProxy {
                 return 12889745;
             }
         }, AtumBlocks.DEAD_GRASS);
-        MinecraftForge.EVENT_BUS.register(EntityDesertWolf.class);
     }
 
     @SubscribeEvent
