bb30f75250e6f7d60ba33aec6606ac0fa019fdf9
GirafiStudios <faj10@msn.com>
2020-10-27 11:47:08 +0100

    Register Surface builders to Forge registry as well

--- src/main/java/com/teammetallurgy/atum/world/gen/AtumSurfaceBuilders.java
+++ src/main/java/com/teammetallurgy/atum/world/gen/AtumSurfaceBuilders.java
@@ -10,11 +10,17 @@ import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.ISurfaceBuilderConfig;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Atum.MOD_ID)
 public class AtumSurfaceBuilders {
     //Surface Builders
+    private static final List<SurfaceBuilder<?>> SURFACE_BUILDERS = new ArrayList<>();
     public static final SurfaceBuilder<SurfaceBuilderConfig> OASIS_SURFACE_BUILDER = registerBuilder("oasis", new OasisSurfaceBuilder(SurfaceBuilderConfig.field_237203_a_));
 
     //States
@@ -31,17 +37,27 @@ public class AtumSurfaceBuilders {
     public static final SurfaceBuilderConfig OASIS_CONFIG = new SurfaceBuilderConfig(FERTILE_SOIL, SAND, FERTILE_SOIL);
 
     //Configured Surface Builders
-    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> SANDY = register("sandy", SurfaceBuilder.DEFAULT.func_242929_a(SANDY_CONFIG));
-    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> SANDY_LIMESTONE = register("sandy_limestone", SurfaceBuilder.DEFAULT.func_242929_a(SANDY_LIMESTONE_CONFIG));
-    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> GRAVEL_CRACKED = register("gravel_cracked", SurfaceBuilder.DEFAULT.func_242929_a(GRAVEL_CRACKED_CONFIG));
-    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> OASIS = register("oasis", OASIS_SURFACE_BUILDER.func_242929_a(OASIS_CONFIG));
+    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> SANDY = registerConfig("sandy", SurfaceBuilder.DEFAULT.func_242929_a(SANDY_CONFIG));
+    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> SANDY_LIMESTONE = registerConfig("sandy_limestone", SurfaceBuilder.DEFAULT.func_242929_a(SANDY_LIMESTONE_CONFIG));
+    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> GRAVEL_CRACKED = registerConfig("gravel_cracked", SurfaceBuilder.DEFAULT.func_242929_a(GRAVEL_CRACKED_CONFIG));
+    public static final ConfiguredSurfaceBuilder<SurfaceBuilderConfig> OASIS = registerConfig("oasis", OASIS_SURFACE_BUILDER.func_242929_a(OASIS_CONFIG));
 
     private static <C extends ISurfaceBuilderConfig, F extends SurfaceBuilder<C>> F registerBuilder(String name, F builder) {
         ResourceLocation id = new ResourceLocation(Atum.MOD_ID, name);
-        return Registry.register(Registry.SURFACE_BUILDER, id, builder);
+        builder.setRegistryName(id);
+        SURFACE_BUILDERS.add(builder);
+        Registry.register(Registry.SURFACE_BUILDER, id, builder);
+        return builder;
     }
 
-    private static <SC extends ISurfaceBuilderConfig> ConfiguredSurfaceBuilder<SC> register(String name, ConfiguredSurfaceBuilder<SC> csb) {
+    private static <SC extends ISurfaceBuilderConfig> ConfiguredSurfaceBuilder<SC> registerConfig(String name, ConfiguredSurfaceBuilder<SC> csb) {
         return WorldGenRegistries.register(WorldGenRegistries.CONFIGURED_SURFACE_BUILDER, name, csb);
+    }
+
+    @SubscribeEvent
+    public static void registerSurfaceBuilders(RegistryEvent.Register<SurfaceBuilder<?>> event) {
+        for (SurfaceBuilder<?> surfaceBuilder : SURFACE_BUILDERS) {
+            event.getRegistry().register(surfaceBuilder);
+        }
     }
 }
