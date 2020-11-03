f65f5297e59f6b6ed8e8412fc78dd743708349e3
GirafiStudios <faj10@msn.com>
2020-11-03 20:58:02 +0100

    Fix Biome provider seed

--- src/main/java/com/teammetallurgy/atum/world/biome/AtumBiomeProvider.java
+++ src/main/java/com/teammetallurgy/atum/world/biome/AtumBiomeProvider.java
@@ -15,26 +15,24 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class AtumBiomeProvider extends BiomeProvider {
     public static final Codec<AtumBiomeProvider> CODEC = RecordCodecBuilder.create((builder) -> {
-        return builder.group(Codec.LONG.fieldOf("seed").stable().forGetter((atumBiomeProvider) -> {
-            return atumBiomeProvider.seed;
-        }), Codec.BOOL.fieldOf("large_biomes").orElse(false).stable().forGetter((atumBiomeProvider) -> {
+        return builder.group(Codec.BOOL.fieldOf("large_biomes").orElse(false).stable().forGetter((atumBiomeProvider) -> {
             return atumBiomeProvider.largeBiomes;
         }), RegistryLookupCodec.getLookUpCodec(ForgeRegistries.Keys.BIOMES).forGetter((atumBiomeProvider) -> {
             return atumBiomeProvider.lookupRegistry;
         })).apply(builder, builder.stable(AtumBiomeProvider::new));
     });
     private final Layer genBiomes;
-    private final long seed;
     private final boolean largeBiomes;
     private final Registry<Biome> lookupRegistry;
 
-    public AtumBiomeProvider(long seed, boolean largeBiomes, Registry<Biome> lookupRegistry) {
+    public AtumBiomeProvider(boolean largeBiomes, Registry<Biome> lookupRegistry) {
         super(AtumRegistry.BIOME_KEYS.stream().map(AtumBiomeProvider::getBiome).collect(Collectors.toList()));
-        this.seed = seed;
+        long seed = (new Random()).nextLong(); //Workaround for vanilla bug, not applying seeds to biomes properly. TODO Revisit in 1.17
         this.largeBiomes = largeBiomes;
         this.lookupRegistry = lookupRegistry;
         this.genBiomes = AtumLayerUtil.getNoiseLayer(seed, largeBiomes ? 6 : 4, 6);
@@ -58,7 +56,7 @@ public class AtumBiomeProvider extends BiomeProvider {
     @Nonnull
     @OnlyIn(Dist.CLIENT)
     public BiomeProvider getBiomeProvider(long seed) {
-        return new AtumBiomeProvider(seed, this.largeBiomes, this.lookupRegistry);
+        return new AtumBiomeProvider(this.largeBiomes, this.lookupRegistry);
     }
 
     @Override
