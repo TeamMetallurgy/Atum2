0cf9c7518a4b40e0b7cc9f52553cd85d7c34eb0c
GirafiStudios <faj10@msn.com>
2021-01-11 14:54:39 +0100

    Finished Atem armor recall

--- src/main/java/com/teammetallurgy/atum/items/artifacts/atem/AtemArmor.java
+++ src/main/java/com/teammetallurgy/atum/items/artifacts/atem/AtemArmor.java
@@ -6,38 +6,40 @@ import com.teammetallurgy.atum.api.God;
 import com.teammetallurgy.atum.client.model.armor.AtemArmorModel;
 import com.teammetallurgy.atum.init.AtumItems;
 import com.teammetallurgy.atum.items.artifacts.ArtifactArmor;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import com.teammetallurgy.atum.misc.StackHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.List;
 
+@Mod.EventBusSubscriber(modid = Atum.MOD_ID)
 public class AtemArmor extends ArtifactArmor {
     protected static final HashMap<PlayerEntity, Integer> RECALL_TIMER = new HashMap<>();
 
     public AtemArmor(EquipmentSlotType slot) {
         super(AtumMats.NEBU_ARMOR, "atem_armor", slot, new Item.Properties().rarity(Rarity.RARE));
         this.setHasRender();
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
@@ -72,34 +74,36 @@ public class AtemArmor extends ArtifactArmor {
     }
 
     @SubscribeEvent
-    public void tick(TickEvent.PlayerTickEvent event) {
-        PlayerEntity player = event.player;
+    public static void tick(TickEvent.PlayerTickEvent event) {
+        if (event.side == LogicalSide.SERVER && event.phase == TickEvent.Phase.START) {
+            PlayerEntity player = event.player;
 
-        if (RECALL_TIMER.containsKey(player)) {
-            int recallTimer = RECALL_TIMER.get(player);
-            System.out.println(recallTimer);
-            if (recallTimer == 0) {
-                RECALL_TIMER.remove(player);
-            }
+            if (player instanceof ServerPlayerEntity) {
+                if (RECALL_TIMER.containsKey(player)) {
+                    int recallTimer = RECALL_TIMER.get(player);
+                    System.out.println(recallTimer);
+                    if (recallTimer == 0) {
+                        RECALL_TIMER.remove(player);
+                    }
 
-            if (recallTimer > 0) {
-                RECALL_TIMER.replace(player, recallTimer - 1);
+                    if (recallTimer > 0) {
+                        RECALL_TIMER.replace(player, recallTimer - 1);
+                    }
+                }
             }
         }
     }
 
     @SubscribeEvent
-    public void onLivingDamage(LivingDamageEvent event) {
+    public static void onLivingDamage(LivingDamageEvent event) {
         LivingEntity livingEntity = event.getEntityLiving();
         if (livingEntity instanceof PlayerEntity) {
             PlayerEntity player = (PlayerEntity) livingEntity;
-            if (event.getAmount() >= player.getHealth() && hasFullSet(player)) {
-                System.out.println(!RECALL_TIMER.containsKey(player));
+            if (event.getAmount() >= player.getHealth() && StackHelper.hasFullArmorSet(livingEntity, AtumItems.EYES_OF_ATEM, AtumItems.BODY_OF_ATEM, AtumItems.LEGS_OF_ATEM, AtumItems.FEET_OF_ATEM)) {
                 if (!RECALL_TIMER.containsKey(player)) {
                     livingEntity.heal(1.0F);
-                    System.out.println("I WILL SURVIVE");
                     AtemsHomecomingItem.recall(livingEntity.world, player);
-                    RECALL_TIMER.put(player, 100);
+                    RECALL_TIMER.put(player, 24000);
                 }
             }
         }
@@ -108,11 +112,14 @@ public class AtemArmor extends ArtifactArmor {
     @Override
     @OnlyIn(Dist.CLIENT)
     public void addInformation(@Nonnull ItemStack stack, World world, @Nonnull List<ITextComponent> tooltip, @Nonnull ITooltipFlag flag) {
+        super.addInformation(stack, world, tooltip, flag);
         PlayerEntity player = Minecraft.getInstance().player;
         if (player != null && RECALL_TIMER.containsKey(player)) {
-            tooltip.add(new TranslationTextComponent(Atum.MOD_ID + ".recall_cooldown", RECALL_TIMER.get(player)));
-        }
+            int totalSeconds = RECALL_TIMER.get(player) / 20;
+            int minutes = (totalSeconds % 3600) / 60;
+            int seconds = totalSeconds % 60;
 
-        super.addInformation(stack, world, tooltip, flag);
+            tooltip.add(new TranslationTextComponent(Atum.MOD_ID + ".recall_cooldown", minutes + ":" + seconds).mergeStyle(TextFormatting.GRAY));
+        }
     }
 }
