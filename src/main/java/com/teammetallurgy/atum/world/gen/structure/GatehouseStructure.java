4c367db05727ee7dc6a2e8d2cf9c22c3e6c416a6
GirafiStudios <faj10@msn.com>
2021-05-06 14:56:13 +0200

    Added bandit spawners

--- src/main/java/com/teammetallurgy/atum/world/gen/structure/GatehouseStructure.java
+++ src/main/java/com/teammetallurgy/atum/world/gen/structure/GatehouseStructure.java
@@ -1,18 +1,8 @@
 package com.teammetallurgy.atum.world.gen.structure;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MutableBoundingBox;
-import net.minecraft.util.registry.DynamicRegistries;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
-import net.minecraft.world.gen.feature.jigsaw.JigsawPatternRegistry;
-import net.minecraft.world.gen.feature.structure.AbstractVillagePiece;
 import net.minecraft.world.gen.feature.structure.JigsawStructure;
-import net.minecraft.world.gen.feature.structure.MarginedStructureStart;
 import net.minecraft.world.gen.feature.structure.VillageConfig;
-import net.minecraft.world.gen.feature.template.TemplateManager;
 
 public class GatehouseStructure extends JigsawStructure {
 
