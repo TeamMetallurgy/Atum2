6356ff2450f6a8f2b8ed38584b9c5d976af41150
GirafiStudios <faj10@msn.com>
2022-03-08 21:04:17 +0100

    No z

--- src/main/java/com/teammetallurgy/atum/world/gen/AtumSurfaceBuilders.java
+++ src/main/java/com/teammetallurgy/atum/world/gen/AtumSurfaceBuilders.java
@@ -12,4 +12,3 @@ public class AtumSurfaceBuilders { //TODO Only kept for temporary reference
     public static final SurfaceBuilderBaseConfiguration GRAVEL_CRACKED_CONFIG = new SurfaceBuilderBaseConfiguration(GRAVEL, CRACKED_LIMESTONE, GRAVEL);
     public static final SurfaceBuilderBaseConfiguration OASIS_CONFIG = new SurfaceBuilderBaseConfiguration(FERTILE_SOIL, SAND, FERTILE_SOIL);
 }*/
-z
