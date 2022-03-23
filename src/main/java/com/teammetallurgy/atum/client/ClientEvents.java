b47f9cd50ca9da9d45b840a746e0655b87312ee0
GirafiStudios <faj10@msn.com>
2022-03-23 14:19:01 +0100

    Moved fog rendering to new RenderFogEvent

--- src/main/java/com/teammetallurgy/atum/client/ClientEvents.java
+++ src/main/java/com/teammetallurgy/atum/client/ClientEvents.java
@@ -1,5 +1,6 @@
 package com.teammetallurgy.atum.client;
 
+import com.mojang.blaze3d.shaders.FogShape;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
@@ -13,24 +14,28 @@ import com.teammetallurgy.atum.items.artifacts.nuit.NuitsVanishingItem;
 import com.teammetallurgy.atum.misc.AtumConfig;
 import com.teammetallurgy.atum.world.DimensionHelper;
 import com.teammetallurgy.atum.world.SandstormHandler;
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.material.FogType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
@@ -43,7 +48,7 @@ public class ClientEvents {
     private static final ResourceLocation MUMMY_BLUR_TEXTURE = new ResourceLocation(Atum.MOD_ID, "textures/hud/mummyblur.png");
 
     @SubscribeEvent
-    public static void renderFog(EntityViewRenderEvent.FogDensity event) {
+    public static void renderFog(EntityViewRenderEvent.RenderFogEvent event) {
         float sandstormFog = AtumConfig.SANDSTORM.sandstormFog.get();
         LocalPlayer clientPlayer = Minecraft.getInstance().player;
         if (clientPlayer == null) return;
@@ -51,32 +56,91 @@ public class ClientEvents {
         Level world = entity.level;
 
         if (world.dimension() == Atum.ATUM && AtumConfig.GENERAL.fogEnabled.get()) {
-            event.setCanceled(true); //Needs to get canceled, before it can work
-            float fogDensity = 200.0F;
+            if (event.getMode() == FogRenderer.FogMode.FOG_SKY) {
+                event.setCanceled(true); //Needs to get canceled, before it can work
+                event.setFarPlaneDistance(100.0F);
+                event.setNearPlaneDistance(25.0F);
+            } else if (event.getMode() == FogRenderer.FogMode.FOG_TERRAIN) {
+                float fogDensity = 220.0F;
 
-            if (entity instanceof Player player) {
-                if (player.blockPosition().getY() <= 60) {
-                    fogDensity += (float) (62 - player.blockPosition().getY()) * 0.333F;
-                }
-                Holder<Biome> biome = world.getBiome(player.blockPosition());
-                if (biome.is(AtumBiomes.OASIS.location())) {
-                    fogDensity *= 2.0F;
-                }
+                if (entity instanceof Player player) {
+                    if (player.blockPosition().getY() <= 60) {
+                        fogDensity += (float) (62 - player.blockPosition().getY()) * 0.333F;
+                    }
+                    Holder<Biome> biome = world.getBiome(player.blockPosition());
+                    if (biome.is(AtumBiomes.OASIS.location())) {
+                        fogDensity *= 2.0F;
+                    }
 
-                for (ItemStack armor : player.getArmorSlots()) {
-                    if (armor.getItem() instanceof IFogReductionItem fogReductionItem) {
-                        EquipmentSlot slotType = Mob.getEquipmentSlotForItem(armor);
-                        if (fogReductionItem.getSlotTypes().contains(slotType)) {
-                            fogDensity = fogReductionItem.getFogReduction(fogDensity, armor);
+                    for (ItemStack armor : player.getArmorSlots()) {
+                        if (armor.getItem() instanceof IFogReductionItem fogReductionItem) {
+                            EquipmentSlot slotType = Mob.getEquipmentSlotForItem(armor);
+                            if (fogReductionItem.getSlotTypes().contains(slotType)) {
+                                fogDensity = fogReductionItem.getFogReduction(fogDensity, armor);
+                            }
                         }
                     }
+                    if (player.getY() >= DimensionHelper.GROUND_LEVEL - 8) {
+                        fogDensity *= 1 + sandstormFog - (sandstormFog - sandstormFog * SandstormHandler.INSTANCE.stormStrength);
+                    }
+                    Minecraft mc = Minecraft.getInstance();
+                    if (mc.level != null) {
+                        Camera camera = event.getCamera();
+                        boolean flag2 = mc.level.effects().isFoggyAt(Mth.floor(camera.getPosition().x()), Mth.floor(camera.getPosition().y())) || mc.gui.getBossOverlay().shouldCreateWorldFog();
+                        setupFog(fogDensity, camera, FogRenderer.FogMode.FOG_TERRAIN, Math.max(mc.gameRenderer.getRenderDistance(), 32.0F), flag2);
+                    }
                 }
-                if (player.getY() >= DimensionHelper.GROUND_LEVEL - 8) {
-                    fogDensity *= 1 + sandstormFog - (sandstormFog - sandstormFog * SandstormHandler.INSTANCE.stormStrength);
-                }
-                event.setDensity(fogDensity);
             }
         }
+    }
+
+    public static void setupFog(float baseFog, Camera p_109025_, FogRenderer.FogMode p_109026_, float p_109027_, boolean p_109028_) {
+        FogType fogtype = p_109025_.getFluidInCamera();
+        Entity entity = p_109025_.getEntity();
+        FogShape fogshape = FogShape.SPHERE;
+        float f;
+        float f1;
+        if (baseFog >= 0) {
+            f = -8.0f;
+            f1 = baseFog * 0.5F;
+        } else
+        if (entity instanceof LivingEntity && ((LivingEntity)entity).hasEffect(MobEffects.BLINDNESS)) {
+            int i = ((LivingEntity)entity).getEffect(MobEffects.BLINDNESS).getDuration();
+            float f3 = Mth.lerp(Math.min(1.0F, (float)i / 20.0F), p_109027_, 5.0F);
+            if (p_109026_ == FogRenderer.FogMode.FOG_SKY) {
+                f = 0.0F;
+                f1 = f3 * 0.8F;
+            } else {
+                f = fogtype == FogType.WATER ? -4.0F : f3 * 0.25F;
+                f1 = f3;
+            }
+        } else if (fogtype == FogType.WATER) {
+            f = -8.0F;
+            f1 = 96.0F;
+            if (entity instanceof LocalPlayer localplayer) {
+                f1 *= Math.max(0.25F, localplayer.getWaterVision());
+            }
+            if (f1 > p_109027_) {
+                f1 = p_109027_;
+                fogshape = FogShape.CYLINDER;
+            }
+        } else if (p_109028_) {
+            f = p_109027_ * 0.05F;
+            f1 = Math.min(p_109027_, 192.0F) * 0.5F;
+        } else if (p_109026_ == FogRenderer.FogMode.FOG_SKY) {
+            f = 0.0F;
+            f1 = p_109027_;
+            fogshape = FogShape.CYLINDER;
+        } else {
+            float f2 = Mth.clamp(p_109027_ / 10.0F, 4.0F, 64.0F);
+            f = p_109027_ - f2;
+            f1 = p_109027_;
+            fogshape = FogShape.CYLINDER;
+        }
+
+        RenderSystem.setShaderFogStart(f);
+        RenderSystem.setShaderFogEnd(f1);
+        RenderSystem.setShaderFogShape(fogshape);
     }
 
     @SubscribeEvent
